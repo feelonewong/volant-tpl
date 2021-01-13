86c2d4d865ed9c7e1c063c1460ccfd17fbb40654
feelonewong <feelonewong@gmail.com>
2021-01-13 16:02:02 +0800

    import download-git-repo

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 const program = require("commander");
-const versionOfPackage  = require("./package.json").version;
 const suggestedOptions = require("./lib/core/help");
+const createCommand = require("./lib/core/create");
+const versionOfPackage  = require("./package.json").version;
 
 program.version(versionOfPackage);
+
+//about sugestion
 suggestedOptions();
 
-
+//about terminal command
+createCommand();
 
 
 program.parse(process.argv);
